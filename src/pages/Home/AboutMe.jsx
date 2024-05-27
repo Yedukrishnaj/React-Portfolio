8835a75996f1afd429e8e50c5c52132fda733554
Yedukrishnaj <115890774+Yedukrishnaj@users.noreply.github.com>
2024-05-27 12:50:07 +0100

    Portfolio

---
+++ src/pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe(){
+    return(
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png"alt="About Me"/>
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills-section--heading">AboutMe</h1>
+                    <p className="hero--section--description">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni voluptatibus possimus corporis excepturi necessitatibus velit ea eius modi perferendis dignissimos veniam non temporibus doloremque soluta, ducimus recusandae. In, doloremque laborum?
+                    </p>
+                    <p className="hero--section--description">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni voluptatibus possimus corporis excepturi necessitatibus velit ea eius modi perferendis dignissimos veniam non temporibus doloremque soluta, ducimus recusandae. In, doloremque laborum?
+                    </p>
+                    </div>
+                    </div>        
+            </section>
+    );
+}
--- src/pages/Home/Homescreen/index.jsx
+++ src/pages/Home/Homescreen/index.jsx
@@ -1,10 +1,14 @@
+import AboutMe from "../AboutMe";
 import Hero from "../Hero";
+import MyPortfolio from "../MyPortfolio";
 import Skills from  "../Skills";
 export default function Home () {
     return (
         <>
         <Hero/>
         <Skills/>
+        <AboutMe/>
+        <MyPortfolio/>
         </>
     )
 }

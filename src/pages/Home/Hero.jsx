d8e79fcc83b28eb64dce6713367749d798933beb
Yedukrishnaj <115890774+Yedukrishnaj@users.noreply.github.com>
2024-12-18 12:42:27 +0000

    photo

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -5,20 +5,20 @@ export default function Hero() {
                 <div className="hero-section-content">
                     <p className="section-title">Hey, I`m Yedu</p>
                     <h1 className="hero-section-title">
-                        <span className="hero-section-title-color">Full Stack</span>{""}
+                        <span className="hero-section-title-color">Computing and IT Management</span>{""}
                         <br/>
-                        Developer
+                        Graduate
                     </h1>
-                    <p className="hero-section-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    <br/>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic ipsa a sapiente!
+                    <p className="hero-section-description">Hello, I am a Masters graduate from Cardiff University. I have a strong passion for Computers and I am always eager to learn new skills and technologies related to my field of study. During my time as a student, I have gained valuable experience through various projects. These projects have helped me to improve my technical skills, work effectively in a team, and manage projects from start to finish. In my spare time, I enjoy watching movies and playing video games. Thank you for visiting my website!
                     </p>
                 </div>
                 <button className="btn btn-primary">Get in Touch</button>
             </div>
             <div className="hero-section-img">
-                <img src="./img/hero_img.png " alt="Hero Section"/>
+                <img src="./img/daqf.jpg" alt="Hero Section"/>
             </div>
         </section>
+        
     )
     ;
 }
